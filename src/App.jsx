800a6a976a5f02fa8dc526ae1ae9ee8a92f03723
BrenoLemesD <brenolemesd@gmail.com>
2024-10-16 15:00:32 -0300

    Configurando o React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import { useState } from "react";
 import "./App.css";
 
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+//pages
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <h1>MiniBlog</h1>
+      <div className="App">
+        <BrowserRouter>
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
       </div>
     </>
   );
