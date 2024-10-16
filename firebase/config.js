a6d09382b3b94ef36bfc2d77984a390d38c269c2
BrenoLemesD <brenolemesd@gmail.com>
2024-10-16 14:45:16 -0300

    Criando o projeto com vite

---
+++ firebase/config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDC25bl7xMgt3CDSu4__PVHUQaOaC0nEdo",
+  authDomain: "miniblog-11a77.firebaseapp.com",
+  projectId: "miniblog-11a77",
+  storageBucket: "miniblog-11a77.appspot.com",
+  messagingSenderId: "1069828274496",
+  appId: "1:1069828274496:web:a65a9125b5d1f9938f62a5",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+//exportando o banco de dados
+export { db };
