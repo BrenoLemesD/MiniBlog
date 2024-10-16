8f69aecffb1f2ff3980dcd2c30c13873739d2f07
BrenoLemesD <brenolemesd@gmail.com>
2024-10-16 15:12:41 -0300

    Adicionando componente NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 
+//components
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
 function App() {
   const [count, setCount] = useState(0);
 
@@ -14,12 +18,14 @@ function App() {
     <>
       <div className="App">
         <BrowserRouter>
+          <NavBar />
           <div className="container">
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
             </Routes>
           </div>
+          <Footer />
         </BrowserRouter>
       </div>
     </>
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <Footer>
+      <h3>Escreva sobre o que você tem interesse!</h3>
+      <p>Mini Blog &copy; 2024</p>
+    </Footer>
+  );
+};
+
+export default Footer;
